df9111a468afc0cf392cfa185c3866ac706baec8
yanyan95 <yl666@scarletmail.rutgers.edu>
2016-09-14 10:07:49 -0400

    still doing Moving the Player...

---
+++ Assets/Material/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	void Update () 			///where most of our game code goes!
+	{
+		
+	}
+
+	void FixedUpdate () 	///where physics code goes! 
+	{
+
+	}
+}
