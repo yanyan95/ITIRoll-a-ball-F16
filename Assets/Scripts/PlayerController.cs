3fc6316c12d9599ab338192af13e9095758e4ff4
yanyan95 <yl666@scarletmail.rutgers.edu>
2016-09-15 15:06:20 -0400

    finished Displaying the Score and Text!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
-	private Rigidbody rb;
+    private Rigidbody rb;
     private int count;
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
         count = 0;
-	}
+        SetCountText();
+        winText.text = "";
+    }
 
-	void FixedUpdate () 	///where physics code goes! 
+    void FixedUpdate() 	///where physics code goes! 
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
-	}
+        rb.AddForce(movement * speed);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -30,6 +35,16 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+            SetCountText();
         }
     }
+
+    void SetCountText()
+    { 
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+        }
 }
